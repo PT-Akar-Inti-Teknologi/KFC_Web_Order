b0f4bb1e83d0c4745bc2f99b9104998f0a84e16d
rse <rokhmad.setiawan@akarinti.tech>
2024-08-05 06:30:38 +0700

    chores(storybook): add viewport addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,8 +2,6 @@ import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
   stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
     "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
@@ -12,6 +10,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-viewport"
   ],
   framework: {
     name: "@storybook/nextjs",
