4e490864601a97f7ef2393b8fab552b47a5397d2
rse <rokhmad.setiawan@akarinti.tech>
2024-08-06 08:43:27 +0700

    chores(icons): add new icons

--- components/icons/icon-collections/profile-icons/CustomerServiceIcon.tsx
+++ components/icons/icon-collections/profile-icons/CustomerServiceIcon.tsx
@@ -26,8 +26,8 @@ export default function CustomerServiceIcon(props: IconProps) {
         strokeLinejoin="round"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M6.41707 13.5825C0.66875 7.83352 1.48615 5.20096 2.09212 4.35263C2.16997 4.21552 4.08873 1.34323 6.14549 3.02839C11.2507 7.23288 4.78758 6.63843 9.0745 10.9261C13.3623 15.2128 12.7669 8.74996 16.9716 13.8541C18.6568 15.9117 15.7844 17.8303 15.6482 17.9073C14.7998 18.5141 12.1663 19.3315 6.41707 13.5825Z"
         stroke="currentColor"
         strokeWidth="1.5"
--- components/icons/icon-collections/profile-icons/MyProfileIcon.tsx
+++ components/icons/icon-collections/profile-icons/MyProfileIcon.tsx
@@ -12,8 +12,8 @@ export default function MyProfileIcon(props: IconProps) {
       {...props}
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M9.9873 12.7885C6.76428 12.7885 4.0119 13.2758 4.0119 15.2273C4.0119 17.1789 6.74682 17.6837 9.9873 17.6837C13.2103 17.6837 15.9619 17.1956 15.9619 15.2448C15.9619 13.294 13.2278 12.7885 9.9873 12.7885Z"
         stroke="currentColor"
         strokeWidth="1.5"
@@ -21,8 +21,8 @@ export default function MyProfileIcon(props: IconProps) {
         strokeLinejoin="round"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M9.9873 10.0049C12.1024 10.0049 13.8167 8.28978 13.8167 6.1747C13.8167 4.05962 12.1024 2.34534 9.9873 2.34534C7.87222 2.34534 6.15714 4.05962 6.15714 6.1747C6.15 8.28264 7.85317 9.99772 9.96032 10.0049H9.9873Z"
         stroke="currentColor"
         strokeWidth="1.42857"
